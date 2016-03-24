1e1f2da1736322657355c0b65732afc139a14488
Anders Wallenquist <anders.wallenquist@vertel.se>
2016-03-24 07:21:06 +0100

    edi_route: some changes in cron-jb

--- edi_route/edi_route.py
+++ edi_route/edi_route.py
@@ -48,7 +48,19 @@ class edi_envelope(models.Model):
     def transform(self):
         pass
         
-        
+            
+    def _cron_job_in(self,cr,uid, edi, context=None):
+        edi.write({'to_import': False})
+
+    def _cron_job_out(self,cr,uid, edi, context=None):
+        edi.write({'to_export': False})
+
+    
+    @api.v7
+    def cron_job(self, cr, uid, context=None):
+        for edi in self.pool.get('edi.message').browse(cr, uid, self.pool.get('edi.message').search(cr, uid, [('to_export','=',True)])):
+            edi._cron_job_out(cr,uid,edi,context=context)
+
 class edi_message(models.Model):
     _name = 'edi.message' 
     _inherit = ['mail.thread']
