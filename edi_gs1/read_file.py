b8bcf638cb7daa825c3fbea4cc4fd12686730bbc
Haojun Zou <apollo_zhj@msn.com>
2016-05-31 14:31:40 +0200

    read_file: updated read_file that write salesman to user_id field

--- edi_gs1/read_file.py
+++ edi_gs1/read_file.py
@@ -21,10 +21,11 @@ def _get_store_chef(chef, parent_id):
     if len(c) == 0:
         odoo.env['res.partner'].create({'name': chef, 'function': u'Butikchef', 'parent_id': parent_id, 'use_parent_address': True})
 
-def _get_salesman(salesman):
+def _get_salesman(salesman, parent_id):
     s = odoo.env['res.users'].search([('name', '=', salesman)])
     if len(s) == 0:
-        odoo.env['res.users'].create({'name': salesman, 'login': salesman})
+        odoo.env['res.users'].create({'name': salesman, 'login': salesman, 'sel_groups_9_40_10', '9'})
+    odoo.env['res.partner'].write(parent_id, {'user_id': odoo.env['res.users'].search([('name', '=', salesman)])[0]})
 
 def _get_id_from_gln(gs1_gln):
     p = odoo.env['res.partner'].read(odoo.env['res.partner'].search([('gs1_gln', '=', gs1_gln)]),['id'])
@@ -37,21 +38,21 @@ for i in range(1, 2160):
     gs1_gln = line.get(u'LOKKOD')
     if gs1_gln != 'None':
         partner_values = {
-            'customer_no': line.get(u'IDNR'),
-            'name': line.get(u'JURIDNAMN'),
-            'street': line.get(u'BESADR'),
-            'zip': line.get(u'POSTNR1'),
-            'city': line.get(u'ORT1'),
-            'phone': line.get(u'TELEFON'),
-            'fax': line.get(u'FAX'),
-            'areg': line.get(u'AREG'),
-            'role': line.get(u'KEDJATXT'),
-            'vat': 'SE' + line.get(u'ORGNR') + '01',
-            'gs1_gln': line.get(u'LOKKOD'),
-            'category_id': [(6, False, _get_categ_id(line.get(u'REGION')))],
-            'store_class': line.get(u'BUTIKSKLASS') if line.get(u'BUTIKSKLASS') != 'None' else '',
-            'is_company': True,
-            'size': int(line.get('STORLEK') if line.get('STORLEK') != 'None' else 0) * 1.0,
+            #~ 'customer_no': line.get(u'IDNR'),
+            #~ 'name': line.get(u'JURIDNAMN'),
+            #~ 'street': line.get(u'BESADR'),
+            #~ 'zip': line.get(u'POSTNR1'),
+            #~ 'city': line.get(u'ORT1'),
+            #~ 'phone': line.get(u'TELEFON'),
+            #~ 'fax': line.get(u'FAX'),
+            #~ 'areg': line.get(u'AREG'),
+            #~ 'role': line.get(u'KEDJATXT'),
+            #~ 'vat': 'SE' + line.get(u'ORGNR') + '01',
+            #~ 'gs1_gln': line.get(u'LOKKOD'),
+            #~ 'category_id': [(6, False, _get_categ_id(line.get(u'REGION')))],
+            #~ 'store_class': line.get(u'BUTIKSKLASS') if line.get(u'BUTIKSKLASS') != 'None' else '',
+            #~ 'is_company': True,
+            #~ 'size': int(line.get('STORLEK') if line.get('STORLEK') != 'None' else 0) * 1.0,
             #~ 'fgs_paolos': int(line.get('FSG PAOLOS') if line.get('FSG PAOLOS') != 'None' else 0) * 1.0,
             #~ 'fgs_leroy': int(line.get('FSG LERÖY') if line.get('FSG LERÖY') != 'None' else 0) * 1.0,
         }
@@ -64,7 +65,7 @@ for i in range(1, 2160):
             print 'Partner create'
             odoo.env['res.partner'].create(partner_values)
         _get_store_chef(line.get(u'BUTIKSCHEF'), _get_id_from_gln(gs1_gln))
-        _get_salesman(line.get(u'SÄLJARE'))
+        _get_salesman(line.get(u'SÄLJARE'), _get_id_from_gln(gs1_gln))
 
 
 
