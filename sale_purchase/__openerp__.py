f8c7515452f0593f3c4a505aad7224923175a30b
Anders Wallenquist <aw@leia.vertel.se>
2016-06-08 17:17:29 +0200

    edi_route_purchase: new module sale_purchase: new module

---
+++ sale_purchase/__openerp__.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Sales and Purshase Management',
+    'version': '1.0',
+    'category': 'Hidden',
+    'summary': 'Quotation, Sale Orders, Purshase & Invoicing Control',
+    'description': """
+Manage sales quotations and orders
+==================================
+
+This module makes the link between the sales and purchase management applications
+
+""",
+    'author': 'Vertel AB',
+    'website': 'https://www.vertel.se',
+    'depends': ['sale', 'purshase'],
+    'data': [
+        'sale_purshase_view.xml',
+        'sale_purshase_workflow.xml',
+        'purshase_view.xml',
+    ],
+    'installable': True,
+    'auto_install': True,
+}
