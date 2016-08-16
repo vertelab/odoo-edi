b9ce867bf10a7d47e028d25500b282a5fc5d26c6
Haojun Zou <apollo_zhj@msn.com>
2016-08-16 17:05:55 +0200

    portal_edi: new module added

---
+++ portal_edi/__openerp__.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+{
+    'name': 'Portal EDI',
+    'version': '0.1',
+    'category': 'Tools',
+    'complexity': 'easy',
+    'summary': 'EDI rights for portal users',
+    'description': """
+This module adds edi rights for suppliers
+=========================================
+
+This module installs automatically when you install portal and edi_route.
+
+    """,
+    'author': 'Vertel AB',
+    'depends': ['edi_route','portal'],
+    'data': [
+        'security/ir.model.access.csv',
+        'security/portal_security.xml',
+    ],
+    'installable': True,
+    'auto_install': True,
+    'category': 'Hidden',
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
