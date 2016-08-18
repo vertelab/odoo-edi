a78bfeb7edc912f2fcc300258e1755c356f05f15
Haojun Zou <apollo_zhj@msn.com>
2016-08-18 13:57:46 +0200

    edi_gs1: added extra control for kontrakt of customer

--- edi_gs1/edi_route.py
+++ edi_gs1/edi_route.py
@@ -201,6 +201,8 @@ class edi_message(models.Model):
 
     def _get_contract(self, ref):
         contract = self.env['account.analytic.account'].search([('code', '=', ref)])
+        if len(contract) > 1:
+            contract = contract[0]
         _logger.info('_get_contract %s %s' % (contract, contract.id))
         if contract:
             return contract.id
