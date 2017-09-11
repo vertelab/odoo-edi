1c6cc61dcebaf5ebbe66b5203baac1372cc57bfb
Anders Wallenquist <anders.wallenquist@vertel.se>
2017-09-11 17:43:46 +0200

    edi_route_mail: finds an invoice

--- edi_route_mail/edi_route.py
+++ edi_route_mail/edi_route.py
@@ -29,6 +29,8 @@ import fnmatch
 import zipfile
 import base64
 
+import re
+
 import logging
 _logger = logging.getLogger(__name__)
 
@@ -36,86 +38,80 @@ class edi_envelope(models.Model):
     _inherit = 'edi.envelope'
 
     mail_id = fields.Many2one(comodel_name="mail.message")
+    route_type = fields.Selection(selection_add=[('mail','Mail')])
+    
+    
+    def _check_mail_attachments(self):
+        for attachment in self.mail_id.attachment_ids:
+            if attachment.mimetype == 'application/pdf':
+                pass # attachment.pdf2image(800,1200)
+            elif attachment.mimetype in ['image/jpeg','image/png','image/gif']:
+                pass
+    def _check_mail_pattern(self):
+        txt = self.mail_id.subject + self.mail_id.body
+        for attachment in self.mail_id.attachment_ids:
+            txt += attachment.index_content
+        m = re.search(self.pattern,txt)
+        return m.group(0)
+    
 
     @api.one
     def split(self):
-        mail = self.env['mail.message'].search([('model','=','edi.envelope'),('res_id','=',self.id),('type','=','email')])
-        self.mail_id = mail.id
-        attachments = self.env['ir.attachment'].search([('res_model','=','edi.envelope'),('res_id','=',self.id)])
-        if len(attachments)>0:
-            import magic  #  pip install filemagic
-            if 'PDF' in magic.Magic().id_buffer(attachments[0].datas.decode('base64')):
-                _logger.warning('filetype %s' % magic.Magic().id_buffer(attachments[0].datas.decode('base64')))
-                message = self.env['edi.message'].create({
-                    'name': self.env['ir.sequence'].next_by_id(self.env.ref('edi_route.sequence_edi_message').id),
-                    'envelope_id': self.id,
-                    'consignor_id': mail.author_id and mail.author_id.id or False,
-                    'route_type': self.route_id.route_type,
-                    'route_id': self.route_id.id,
-                    })
-                message.unpack()  # Preserv the pdffile as an attachment
-                attachments[0].write({'res_model' : message.model, 'res_id': message.res_id})
-            else:
-                _logger.warning('no PDF filetype %s' % magic.Magic().id_buffer(attachments[0].datas.decode('base64')))
-                message = self.env['edi.message'].create({
-                    'name': self.env['ir.sequence'].next_by_id(self.env.ref('edi_route.sequence_edi_message').id),
-                    'envelope_id': self.id,
-                    'consignor_id': mail.author_id and mail.author_id.id or False,
-                    'route_type': self.route_id.route_type,
-                    'route_id': self.route_id.id,
-                    'body':  attachments[0].datas,
-                    })
-                message.unpack()
-            self.env['mail.message'].create({
-                    'body': _("{type} <a href='/web#model={model}&id={id}'>{message}</a> created\n").format(type='invoice_pdf',model=mail._name,id=mail.id,message=mail.name),
-                    'subject': mail.subject,
-                    'author_id': self.env['res.users'].browse(self.env.uid).partner_id.id,
-                    'res_id': self.id,
-                    'model': self._name,
-                    'type': 'notification',})
+        self.mail_id = self.env['mail.message'].search([('model','=','edi.envelope'),('res_id','=',self.id),('type','=','email')])
+        self.sender = self.mail_id.author_id
+        self._check_mail_attachments()  # 
+        key = self._check_mail_pattern()
+
+        record = self.env[self.model_id.name].search(['|',('name','=',key),('reference_type','=',key)])
+        if record:
+            for attachment in self.mail_id.attachment_ids:
+                attachment.res_model = record._name
+                attachment.res_id = record.id
+        
         super(edi_envelope,self).split()
 
-class edi_message(models.Model):
-    _inherit='edi.message'
+#~ class edi_message(models.Model):
+    #~ _inherit='edi.message'
+    #~ route_type = fields.Selection(selection_add=[('mail','Mail')])
+    
+    #~ @api.one
+    #~ def unpack(self):
+        #~ _logger.warning('unpack (edi_route_mail) %s %s' % (self.edi_type.name, self))
+        #~ if self.edi_type.id == self.env.ref('edi_route_mail.edi_message_type_invoice_pdf'):
+            #~ invoice = self.env['account.invoice'].create({
+                #~ 'name': self.envelope_id.mail_id.subject,
+                #~ 'origin': _('mail'),
+                #~ 'type': 'in_invoice',
+                #~ 'reference': self.envelope_id.mail_id.subject,
+                #~ 'account_id': self.consignor_id.property_account_receivable.id,
+                #~ 'partner_id': self.consignor_id.id,
+                #~ 'journal_id': self.env['account.invoice'].default_get(['journal_id'])['journal_id'],
+                #~ 'currency_id': self.consignor_id.property_product_pricelist and self.consignor_id.property_product_pricelist.currency_id.id or False,
+                #~ 'comment': self.envelope_id.mail_id.body,
+                #~ 'payment_term': self.consignor_id.property_payment_term and self.consignor_id.property_payment_term.id or False,
+                #~ 'fiscal_position': self.consignor_id.property_account_position and self.consignor_id.property_account_position.id or False,
+                #~ 'date_invoice': self.envelope_id.mail_id.date,
+                #~ 'company_id': self.env['res.users']._get_company(),
+                #~ 'user_id': self.consignor_id.user_id and self.consignor_id.user_id.id or self.env.uid,
+            #~ })
+            #~ self.write({'model': 'account.invoice', 'res_id': invoice.id})
+            #~ self.env['mail.message'].create({
+                    #~ 'body': _("{type} <a href='/web#model={model}&id={id}'>{message}</a> created\n").format(type=self.edi_type.name,model=self._name,id=self.id,message=self.name),
+                    #~ 'subject': self.edi_type.name,
+                    #~ 'author_id': self.env['res.users'].browse(self.env.uid).partner_id.id,
+                    #~ 'res_id': invoice.id,
+                    #~ 'model': invoice._name,
+                    #~ 'type': 'notification',})
+            #~ self.env['mail.message'].create({
+                    #~ 'body': _("{type} <a href='/web#model={model}&id={id}'>{message}</a> created\n").format(type='invoice_pdf',model=invoice._name,id=invoice.id,message=invoice.name),
+                    #~ 'subject': self.edi_type.name,
+                    #~ 'author_id': self.env['res.users'].browse(self.env.uid).partner_id.id,
+                    #~ 'res_id': self.id,
+                    #~ 'model': self._name,
+                    #~ 'type': 'notification',})
 
-    @api.one
-    def unpack(self):
-        _logger.warning('unpack (edi_route_mail) %s %s' % (self.edi_type.name, self))
-        if self.edi_type.id == self.env.ref('edi_route_mail.edi_message_type_invoice_pdf'):
-            invoice = self.env['account.invoice'].create({
-                'name': self.envelope_id.mail_id.subject,
-                'origin': _('mail'),
-                'type': 'in_invoice',
-                'reference': self.envelope_id.mail_id.subject,
-                'account_id': self.consignor_id.property_account_receivable.id,
-                'partner_id': self.consignor_id.id,
-                'journal_id': self.env['account.invoice'].default_get(['journal_id'])['journal_id'],
-                'currency_id': self.consignor_id.property_product_pricelist and self.consignor_id.property_product_pricelist.currency_id.id or False,
-                'comment': self.envelope_id.mail_id.body,
-                'payment_term': self.consignor_id.property_payment_term and self.consignor_id.property_payment_term.id or False,
-                'fiscal_position': self.consignor_id.property_account_position and self.consignor_id.property_account_position.id or False,
-                'date_invoice': self.envelope_id.mail_id.date,
-                'company_id': self.env['res.users']._get_company(),
-                'user_id': self.consignor_id.user_id and self.consignor_id.user_id.id or self.env.uid,
-            })
-            self.write({'model': 'account.invoice', 'res_id': invoice.id})
-            self.env['mail.message'].create({
-                    'body': _("{type} <a href='/web#model={model}&id={id}'>{message}</a> created\n").format(type=self.edi_type.name,model=self._name,id=self.id,message=self.name),
-                    'subject': self.edi_type.name,
-                    'author_id': self.env['res.users'].browse(self.env.uid).partner_id.id,
-                    'res_id': invoice.id,
-                    'model': invoice._name,
-                    'type': 'notification',})
-            self.env['mail.message'].create({
-                    'body': _("{type} <a href='/web#model={model}&id={id}'>{message}</a> created\n").format(type='invoice_pdf',model=invoice._name,id=invoice.id,message=invoice.name),
-                    'subject': self.edi_type.name,
-                    'author_id': self.env['res.users'].browse(self.env.uid).partner_id.id,
-                    'res_id': self.id,
-                    'model': self._name,
-                    'type': 'notification',})
-
-        else:
-            super(edi_message, self).unpack()
+        #~ else:
+            #~ super(edi_message, self).unpack()
 
 
 class edi_route(models.Model):
@@ -129,6 +125,9 @@ class edi_route(models.Model):
     mail_debug = fields.Boolean(string="Debug",required=False)
     protocol = fields.Selection(selection_add=[('mail','Mail')])
     route_type = fields.Selection(selection_add=[('mail','Mail')])
+    pattern = fields.Char(string="Pattern",help="pattern for example customer reference in a supplier invoice, eg PO\d{5}")
+    model_defaults = fields.Text()
+    model_id = fields.Many2one(comodel_name="ir.model")
 
     @api.one
     def run(self):
