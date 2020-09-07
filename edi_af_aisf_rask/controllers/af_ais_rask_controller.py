ab01aca16d3829f097509783df8d4fc82ee0f731
Hans Olofsson <hans.olofsson@arbetsformedlingen.se>
2020-09-07 10:58:38 +0200

    Ändrade filerna...

--- edi_af_aisf_rask/controllers/af_ais_rask_controller.py
+++ edi_af_aisf_rask/controllers/af_ais_rask_controller.py
@@ -5,12 +5,13 @@ from odoo import models, api
 _logger = logging.getLogger(__name__)
 
 class ais_as_rask_controller(models.Model):
-    inherit = 'res.partner'
+    _inherit = 'res.partner'
 
     @api.model
     def rask_controller(self, customerId, socialSecurityNumber, formerSocialSecurityNumber, messageType):
         if messageType == "PersonnummerByte":
-            res_partner_obj = self.env['res.partner'].search([('customerId', '=', customerId)])
+            _logger.info("ais_as_rask_controller.rask_controller(): messageType %s" % messageType)
+            res_partner_obj = self.env['res.partner'].search([('customer_id', '=', customerId)])
             if res_partner_obj:
                 res_partner_obj.company_registry = socialSecurityNumber
             else:
