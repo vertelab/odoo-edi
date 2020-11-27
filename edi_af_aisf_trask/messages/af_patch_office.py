a6689b0a3dea704d9205a7173df3dafbbb6c783a
Daniel Eriksson <daniel.eriksson@arbetsformedlingen.se>
2020-11-27 09:44:57 +0100

    edi_af_aisf_trask,edi_af_appointment AFC-672: pep8 formating

--- edi_af_aisf_trask/messages/af_patch_office.py
+++ edi_af_aisf_trask/messages/af_patch_office.py
@@ -23,26 +23,36 @@ from datetime import datetime, timedelta
 import json
 
 import logging
+
 _logger = logging.getLogger(__name__)
 
+
 class edi_message(models.Model):
-    _inherit='edi.message'
+    _inherit = "edi.message"
 
     @api.one
     def unpack(self):
-        if self.edi_type.id == self.env.ref('edi_af_aisf_trask.asok_patch_office').id:
+        if self.edi_type.id == self.env.ref("edi_af_aisf_trask.asok_patch_office").id:
             pass
         else:
             super(edi_message, self).unpack()
 
     @api.one
     def pack(self):
-        if self.edi_type.id == self.env.ref('edi_af_aisf_trask.asok_patch_office').id:
-            if not self.model_record or self.model_record._name != 'res.partner' or not self.model_record.is_jobseeker:
-                raise Warning("Attached record is not a res.partner or not a jobseeker! {model}".format(model=self.model_record and self.model_record._name or None))
+        if self.edi_type.id == self.env.ref("edi_af_aisf_trask.asok_patch_office").id:
+            if (
+                not self.model_record
+                or self.model_record._name != "res.partner"
+                or not self.model_record.is_jobseeker
+            ):
+                raise Warning(
+                    "Attached record is not a res.partner or not a jobseeker! {model}".format(
+                        model=self.model_record and self.model_record._name or None
+                    )
+                )
 
             obj = self.model_record
-            office_code = self.env.context.get('office_code')
+            office_code = self.env.context.get("office_code")
             if not office_code:
                 try:
                     office_code = obj.office_id.office_code
@@ -50,22 +60,26 @@ class edi_message(models.Model):
                     # leave office_code as False
                     pass
             body_dict = {}
-            body_dict['base_url'] = self.edi_type.type_mapping.format(
-                path = "ais-f-arbetssokande/v2/kontor/{sokande_id}".format(sokande_id = obj.customer_id)
+            body_dict["base_url"] = self.edi_type.type_mapping.format(
+                path="ais-f-arbetssokande/v2/kontor/{sokande_id}".format(
+                    sokande_id=obj.customer_id
+                )
             )
-            body_dict['method'] = 'PATCH'
+            body_dict["method"] = "PATCH"
             data_dict = {}
-            data_dict['ansvarigHandlaggareSignatur'] = obj.user_id.af_signature
+            data_dict["ansvarigHandlaggareSignatur"] = obj.user_id.af_signature
             if office_code:
-                data_dict['kontorsKod'] = office_code
-            body_dict['data'] = data_dict
+                data_dict["kontorsKod"] = office_code
+            body_dict["data"] = data_dict
             self.body = json.dumps(body_dict)
 
-            envelope = self.env['edi.envelope'].create({
-                'name': 'asok office request',
-                'route_id': self.route_id.id,
-                'route_type': self.route_type,
-                'edi_message_ids': [(6, 0, [self.id])]
-            })
+            envelope = self.env["edi.envelope"].create(
+                {
+                    "name": "asok office request",
+                    "route_id": self.route_id.id,
+                    "route_type": self.route_type,
+                    "edi_message_ids": [(6, 0, [self.id])],
+                }
+            )
         else:
             super(edi_message, self).pack()
