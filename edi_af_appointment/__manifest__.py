ea62c1ca81c86fb21245c1d6a1144be37d6d48a6
Daniel Eriksson <47656513+daerio@users.noreply.github.com>
2021-03-19 07:48:32 +0100

    Dev 12.0 afc 1536 (#78)
    * Added support for calls to API with both pnr and customer_nr being sent at the same time.
    * Fixed pep8 formatting
    * Fixed misc

--- edi_af_appointment/__manifest__.py
+++ edi_af_appointment/__manifest__.py
@@ -21,11 +21,13 @@
 
 {
     "name": "EDI AF Appointment",
-    "version": "0.1",
+    "version": "12.0.0.1.1",
     "category": "edi",
     "summary": "EDI AF Appointment - support for appointments ",
     "licence": "AGPL-3",
-    "description": """ """,
+    'description': """
+v12.0.0.1.1 AFC-1536 - Added support for calls to API with both pnr and customer_nr being sent at the same time. \n
+""",
     "author": "Vertel AB",
     "website": "http://www.vertel.se",
     "depends": [
@@ -42,7 +44,6 @@
         "data/edi.ace_errand.csv",
         "views/edi_af_appointment_views.xml",
         "views/edi_message_view.xml",
-        # 'data/res_users.xml',
         "data/scheduled_meeting_reminder.xml",
     ],
     "application": False,
