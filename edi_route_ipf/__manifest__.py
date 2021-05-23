011d8048e0b6808caf454b5db5977024fc910482
Robin Chatfield <robin.chatfield@arbetsformedlingen.se>
2021-05-23 20:47:56 +0200

    AFC-2298 edi_route_ipf, edi_af_appointments: Updated manifests.

--- edi_route_ipf/__manifest__.py
+++ edi_route_ipf/__manifest__.py
@@ -21,7 +21,7 @@
 
 {
     'name': 'EDI Routes IPF',
-    'version': '12.0.2.1.0',
+    'version': '12.0.2.2.0',
     'category': 'edi',
     'summary': 'Routes for EDI using IPF',
     'licence': 'AGPL-3',
@@ -33,6 +33,7 @@ This functionality is tailored for AF. \n
 v12.0.1.0.0: versions before good version control \n
 v12.0.2.0.0 AFC-1766: Major overhaul of how the module works. \n
 v12.0.2.1.0 AFC-1767: Implemented edi.log model. \n
+v12.0.2.2.0 AFC-2298: Fixed edi.log access rights. \n
 \n
 """,
     'author': 'Vertel AB',
