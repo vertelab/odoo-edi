440539bb0320c96874f6a55e413893c587ff462f
Björn Hayer <bjorn.hayer@vertel.se>
2022-04-26 06:52:50 +0000

    Minor bug fixes and some code cleanup

--- edi_peppol_base/models/peppol_base.py
+++ edi_peppol_base/models/peppol_base.py
@@ -33,26 +33,6 @@ class Peppol_Base(models.Model):
     _name = "peppol.base"
     _description = "Base module which contains functions to assist in converting between PEPPOL and Odoo."
 
-    #def test(self):
-    #    _logger.warning("Hej!")
-
-    """
-    def create_SubElement (self, parent, tag, text=None, attri_name=None, attri_value=None):
-        if text == None:
-            nsp = 'cac'
-        else:
-            nsp = 'cbc'
-
-        result = etree.SubElement(parent, QName(NSMAP[nsp], tag))
-        result.text = text
-
-        if attri_name is not None and attri_value is not None:
-            if attri_name != '' and attri_value != '':
-                result.set(attri_name, attri_value)
-
-
-        return result
-    """
 
     def log_element(self, ele, msg='', verbose=False):
         if len(ele) == 0:
@@ -67,33 +47,6 @@ class Peppol_Base(models.Model):
                 str =+ "  | Element Text: " + ele[0].text
             _logger.warning(msg + str)
 
-    """
-    def getfield(self, lookup, inst=None):
-        _logger.warning(inspect.currentframe().f_code.co_name + ": " + "Trying to parse: " + lookup)
-
-        if inst is None:
-            inst = self
-
-        l = lookup.split(',', 1)
-        current_module = l[0].rsplit('.', 1)[0] 
-        current_field_name = l[0].rsplit('.', 1)[1]
-        current_field_value = inst[current_field_name]
-
-        _logger.warning(inspect.currentframe().f_code.co_name + ": " + f"{current_module=}")
-        _logger.warning(inspect.currentframe().f_code.co_name + ": " + f"{current_field_name=}")
-        _logger.warning(inspect.currentframe().f_code.co_name + ": " + f"{current_field_value=}")
-        _logger.warning(inspect.currentframe().f_code.co_name + ": " + f"{len(l)=}")
-
-        if len(l) == 1:
-            return current_field_value
-        else:
-            ln = l[1].split(',', 1)
-            next_module = ln[0].rsplit('.', 1)[0] 
-            _logger.warning(inspect.currentframe().f_code.co_name + ": " + f"{next_module=}")
-            inst = inst.env[next_module].browse(current_field_value.id)
-            _logger.warning(inspect.currentframe().f_code.co_name + ": " + f"{inst=}")    
-            return self.getfield(l[1], inst)
-    """
 
     def convert_to_string(self, value):
         #_logger.warning(f"{type(value)=}")
@@ -106,101 +59,3 @@ class Peppol_Base(models.Model):
 
         _logger.error(inspect.currentframe().f_code.co_name + ": " + "Type of variable is not being handled like it should!")
         return None
-        
-    """
-
-    def convert_field(  self,
-                        tree,
-                        special_function,
-                        fullParent, 
-                        tag, 
-                        static_text=None, 
-                        datamodule=None, 
-                        datamodule_field=None, 
-                        attribute_name=None, 
-                        attributeFixedText=None, 
-                        attirbute_datamodule=None, 
-                        attribute_datamodule_field=None):
-
-    #Ensure that the full Parent path exists
-        parents = fullParent.split('/')
-
-        path = '/'
-        for parent in parents:
-            if parent != "Invoice":
-                #print_element(tree.xpath(path + '/' + parent, namespaces=XNS))
-                if len(tree.xpath(path + '/' + parent, namespaces=XNS)) == 0:
-                    self.create_SubElement(tree.xpath(path, namespaces=XNS)[0], parent.split(':')[1])
-            path = path + '/' + parent
-
-    #Add the new element
-        new_element = self.create_SubElement(tree.xpath(path, namespaces=XNS)[0], tag, static_text, attribute_name,  attributeFixedText)
-
-    #Add data from odoo to element
-        if datamodule_field != "":
-            _logger.warning(inspect.currentframe().f_code.co_name + ": " + "Datamodule_field is: " + datamodule_field)
-            value = self.getfield(datamodule_field)
-            _logger.warning(f"{value=}")
-            new_element.text = self.convert_to_string(value)
-                
-    #Run any special functions on the newly created Element
-    #    functionList = special_function.split(',')
-
-    #    for f in functionList:
-    #        #TODO: This could be made into a more 'proper' 'switch case'
-    #        if f == 'standard_currency':
-    #            standard_currency(new_element)
-
-
-    def adjust_instructions(self, header, instructions):
-
-        column = 0
-        for h in header:
-            
-            if header == 'fullParents':
-                pass
-
-        return instructions
-
-
-    def read_CSV(self, filename):
-        file = open (filename)
-        csvreader = csv.reader(file)
-        header = []
-        header = next(csvreader)
-        instructions = []
-        for row in csvreader:
-            instructions.append(row)
-        file.close()
-
-        instructions = self.adjust_instructions(header, instructions)
-
-        #for n in instructions:
-        #    print(n)
-
-        return instructions
-
-
-    def create_invoice(self):
-        invoice = etree.Element("Invoice", nsmap=NSMAP)
-
-        for n in self.read_CSV('/usr/share/odoo-edi/edi_peppol_base/data/instruction.toPeppol.csv'):
-            self.convert_field(invoice, n[1], n[2], n[3], n[4], n[5], n[6], n[7], n[8], n[9], n[10])
-
-        return invoice
-
-
-    def invoice_to_peppol(self):
-        tree = etree.ElementTree(self.env['peppol.toinvoice'].create_invoice())
-        #_logger.warning("XML has ID: " + tree.xpath('/Invoice/cbc:ID/text()', namespaces=XNS)[0])
-        tree.write('/usr/share/odoo-edi/edi_peppol_base/demo/output.xml', xml_declaration=True, encoding='UTF-8', pretty_print=True)
-
-        #_logger.error(inspect.currentframe().f_code.co_name + ": " + "NO VALIDATION IS DONE!")
-        #_logger.warning("Starting validation attemps")
-
-        self.env['peppol.validate'].validate_peppol('/usr/share/odoo-edi/edi_peppol_base/demo/output.xml')
-        #self.env['peppol.validate'].validate_peppol('/usr/share/odoo-edi/edi_peppol_base/demo/base-example.xml')
-        #_logger.warning("Finished validation attemps")
-    """
-#if __name__ == "__main__":
-#    main()    
