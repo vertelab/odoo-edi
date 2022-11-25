2d52c669d7866175b896ad1ba619624f4d5eed0a
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-11-25 15:54:06 +0100

    Update __manifest__.py

--- edi_peppol_base/__manifest__.py
+++ edi_peppol_base/__manifest__.py
@@ -1,9 +1,8 @@
-
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Odoo, Open Source Enterprise Management Solution, third party addon
-#    Copyright (C) 2014-2022 Vertel AB (<http://vertel.se>).
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -16,26 +15,32 @@
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
 {
-    'name': 'EDI PEPPOL base',
-    'summary': 'Base module for sending and reciving PEPPOL',
-    'author': 'Vertel AB',
-    'contributor': '',
-    'maintainer': 'Vertel AB',
-    'repository': 'https://github.com/vertelab/odoo-edi',
-    'category': 'Accounting',
+    'name': 'EDI: Peppol  Base',
     'version': '14.0.0.0.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
-    'license': 'AGPL-3',
-    'website': 'https://vertel.se/apps/odoo-edi/edi_peppol/',
+    'summary': 'Base module for sending and reciving PEPPOL.',
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Accounting',
     'description': """
         Implements a Framework for PEPPOL communication in Odoo.
         14.0.0.0.0 - Initial version
     """,
+    #'sequence': '1',
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-edi/edi_peppol_base',
+    'images': ['static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-edi',
+    # Any module necessary for this one to work correctly
     'depends': ['base', 'l10n_se'],
     'data': ['security/ir.model.access.csv',
             ],
