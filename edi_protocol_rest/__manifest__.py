faec337505585c64eb9446cadaf99c933a607fdc
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-11-25 16:18:15 +0100

    Update __manifest__.py

--- edi_protocol_rest/__manifest__.py
+++ edi_protocol_rest/__manifest__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Odoo, Open Source Enterprise Management Solution, third party addon
-#    Copyright (C) 2014-2021 Vertel AB (<http://vertel.se>).
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -15,18 +15,32 @@
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
 {
-    "name": "EDI Protocol REST",
-    "description": """
+    'name': 'EDI: Protocol REST',
+    'version': '14.0.0.0.0',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'summary': 'Implements a framework for REST calls via EDI in Odoo.',
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Accounting',
+    'description': """
 Implements a framework for REST calls via EDI in Odoo.
 14.0.0.0.0 - Initial version
     """,
-    "category": "EDI",
-    "version": "14.0.0.0.0",
+    #'sequence': '1',
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-edi/edi_protocol_rest',
+    'images': ['static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-edi',
+    # Any module necessary for this one to work correctly
     "depends": [
         "edi_base",
         "rest_base",
@@ -38,6 +52,5 @@ Implements a framework for REST calls via EDI in Odoo.
     "demo": [],
     "installable": True,
     "application": False,
-    "author": "Vertel AB",
-    "website": "www.vertel.se",
 }
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
