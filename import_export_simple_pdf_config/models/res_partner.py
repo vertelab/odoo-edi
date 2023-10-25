9f384a80242f53d2a6eac0e69145caa1ed93bd0e
iammiracle <miracleaayodele@gmail.com>
2023-10-25 14:26:40 +0200

    improved simple pdf configuration import and export

--- import_export_simple_pdf_config/models/res_partner.py
+++ import_export_simple_pdf_config/models/res_partner.py
@@ -4,7 +4,7 @@ from lxml import etree
 
 EXCLUDE_FIELDS = [
     'create_date', '__last__update', 'create_uid', 'write_date', 'write_uid', 'id', '__last_update',
-    'simple_pdf_test_file', 'simple_pdf_test_filename', 'simple_pdf_test_raw_text', 'simple_pdf_test_results'
+    'simple_pdf_test_file', 'simple_pdf_test_filename', 'simple_pdf_test_raw_text', 'simple_pdf_test_results', 'active'
 ]
 
 
@@ -16,12 +16,38 @@ class Partner(models.Model):
             ('model_id.model', '=', model)
         ]).filtered(filter_func)
 
+    def _process_field_data(self, etree_field, partner_field, res_partner_id):
+        if partner_field.ttype in ['selection', 'char']:
+            etree_field.text = str(getattr(res_partner_id, partner_field.name))
+        if partner_field.ttype == 'many2one':
+            etree_field.text = str(getattr(res_partner_id, partner_field.name).id)
+        if partner_field.ttype in ['one2many', 'many2many']:
+            relation_field_ids = self._get_simple_pdf_fields(
+                model=partner_field.relation,
+                filter_func=lambda r_field: r_field.name not in EXCLUDE_FIELDS,
+            )
+            for line in res_partner_id[partner_field.name]:
+                if partner_field.name == 'tax_ids':
+                    tree_line = etree.SubElement(etree_field, 'line')
+                    tree_line.text = str(line.id)
+                else:
+                    tree_line = etree.SubElement(etree_field, 'line')
+                    for relation_field_id in relation_field_ids:
+                        if line[relation_field_id.name]:
+                            if relation_field_id.ttype == 'many2one':
+                                etree.SubElement(tree_line, relation_field_id.name).text = str(
+                                    line[relation_field_id.name].id
+                                )
+                            else:
+                                etree.SubElement(tree_line, relation_field_id.name).text = str(
+                                    line[relation_field_id.name]
+                                )
+
     def action_export_simple_pdf_config(self):
         active_ids = self.env.context.get('active_ids', [])
         if not active_ids:
             active_ids = self.id
         res_partner_id = self.env['res.partner'].browse(active_ids)
-        print(res_partner_id)
 
         simple_pdf_fields = self._get_simple_pdf_fields(
             model='res.partner',
@@ -32,30 +58,27 @@ class Partner(models.Model):
 
         for partner_field in simple_pdf_fields.filtered(lambda x: x.name not in EXCLUDE_FIELDS):
             if res_partner_id[partner_field.name]:
-                field = etree.SubElement(data, partner_field.name)
-                if partner_field.ttype in ['selection', 'char']:
-                    field.text = str(getattr(res_partner_id, partner_field.name))
-                if partner_field.ttype == 'many2one':
-                    field.text = str(getattr(res_partner_id, partner_field.name).id)
-                if partner_field.ttype in ['one2many', 'many2many']:
-                    relation_field_ids = self._get_simple_pdf_fields(
-                        model=partner_field.relation,
-                        filter_func=lambda r_field: r_field.name not in EXCLUDE_FIELDS,
-                    )
-                    for line in res_partner_id[partner_field.name]:
-                        tree_line = etree.SubElement(field, 'line')
-                        for relation_field_id in relation_field_ids:
-                            if line[relation_field_id.name]:
-                                if relation_field_id.ttype == 'many2one':
-                                    etree.SubElement(tree_line, relation_field_id.name).text = str(
-                                        line[relation_field_id.name].id
-                                    )
-                                else:
-                                    etree.SubElement(tree_line, relation_field_id.name).text = str(
-                                        line[relation_field_id.name]
-                                    )
+                etree_field = etree.SubElement(data, partner_field.name)
+                self._process_field_data(etree_field, partner_field, res_partner_id)
 
+        if res_partner_id.invoice_import_count:
+            self._import_configuration(data, res_partner_id)
         return self._export_attachment(res_partner_id, data)
+
+    def _import_configuration(self, etree_data, res_partner_id):
+        import_config_fields = self._get_simple_pdf_fields(
+            model='account.invoice.import.config',
+            filter_func=lambda module: module.modules == 'account_invoice_import',
+        )
+        import_config_ids = self.env['account.invoice.import.config'].search([('partner_id', '=', res_partner_id.id)])
+        invoice_import_configs_tree = etree.SubElement(etree_data, 'invoice_import_configs')
+
+        for import_config in import_config_ids:
+            invoice_import_config_tree = etree.SubElement(invoice_import_configs_tree, 'invoice_import_config')
+            for import_field in import_config_fields.filtered(lambda x: x.name not in EXCLUDE_FIELDS):
+                if import_config[import_field.name]:
+                    config_etree_field = etree.SubElement(invoice_import_config_tree, import_field.name)
+                    self._process_field_data(config_etree_field, import_field, import_config)
 
     def _export_attachment(self, res_partner_id, data):
         xml_data = etree.tostring(data, pretty_print=True, xml_declaration=True, encoding='utf-8').decode('utf-8')
--- import_export_simple_pdf_config/wizard/export_pdf_wizard.py
+++ import_export_simple_pdf_config/wizard/export_pdf_wizard.py
@@ -13,15 +13,22 @@ class ExportConfiguration(models.Model):
     )
     simple_pdf_filename = fields.Char(string="PDF Config (XML) Filename")
 
-    partner_id = fields.Many2one('res.partner', domain=[('company_type',  '=', 'company')], string="Partner")
+    partner_id = fields.Many2one('res.partner', domain=[('is_company', '=', True)], string="Partner")
 
     def element_to_dict(self, element):
         if len(element) == 0:
             return element.text
 
+        if element[0].tag == 'invoice_import_config':
+            return [self.element_to_dict(child) for child in element]
+
         if element[0].tag == 'line':
             # Handling the case where the element contains multiple 'line' tags
-            return [(0, 0, self.element_to_dict(child)) for child in element]
+            return [
+                (4, int(child.text)) if isinstance(child.text, str) and child.text.isdigit()
+                else (0, 0, self.element_to_dict(child))
+                for child in element
+            ]
 
         # Handling other cases where the element contains various tags
         dictionary = {}
@@ -34,5 +41,17 @@ class ExportConfiguration(models.Model):
 
     def action_export_config(self):
         xml_content = etree.fromstring(base64.b64decode(self.with_context(bin_size=False).simple_pdf_file))
-        self.partner_id.write(self.element_to_dict(xml_content))
+        dict_content = self.element_to_dict(xml_content)
+        if dict_content.get('invoice_import_configs'):
+            invoice_import_configs = dict_content.pop('invoice_import_configs')
+            for config in invoice_import_configs:
+                create_vals = {
+                    **config,
+                    'partner_id': self.partner_id.id,
+                    'name': self.partner_id.name,
+                    'display_name': self.partner_id.name,
+                    'company_id': self.env.user.company_id.id,
+                }
+                self.env['account.invoice.import.config'].create(create_vals)
+        self.partner_id.write(dict_content)
 
