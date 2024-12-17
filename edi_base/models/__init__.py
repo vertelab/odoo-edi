d9d2be087e55dfbb5cacd952e862c29b78b19ecf
iammiracle <miracleaayodele@gmail.com>
2024-12-17 16:53:39 +0100

    migration of import_export_simple_pdf_config

--- edi_base/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import edi_envelope
-from . import edi_log
-from . import edi_log_line
-from . import edi_message
-from . import edi_route
-from . import mixins
--- edi_protocol_rest/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import edi_route
-from . import edi_envelope
-#from . import edi_rest_api
