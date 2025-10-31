4cdad75d92bad7b4b4c49de5a257748b51279116
iammiracle <miracleaayodele@gmail.com>
2025-10-31 11:07:22 +0100

    added smart button to link between edi, purchase and stock picking

--- edi_peppol/__manifest__.py
+++ edi_peppol/__manifest__.py
@@ -36,7 +36,7 @@
     'website': 'https://vertel.se/apps/odoo-',
     'images': ['static/description/banner.png'], # 560x280
     'license': 'AGPL-3',
-    'depends': ['edi_base', 'purchase', 'agreement'],
+    'depends': ['edi_base', 'purchase', 'agreement', 'stock'],
      #"external_dependencies": {
      #   "bin": ["openssl",], 
      #   "python": ["acme_tiny", "IPy",],
