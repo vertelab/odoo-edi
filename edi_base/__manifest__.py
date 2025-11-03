f7fba37791154b9fcd0488e9bd74198afb8cbed6
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2025-11-03 15:40:44 +0100

    T/6468 Update __manifest__.py

--- edi_base/__manifest__.py
+++ edi_base/__manifest__.py
@@ -22,7 +22,7 @@
 # https://www.odoo.com/documentation/14.0/reference/module.html
 #
 {
-    'name': 'EDI Base',
+    'name': 'EDI: Base',
     'version': '1.0',
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
