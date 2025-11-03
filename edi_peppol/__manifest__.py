9032d463e40e3a8b7d21e48fa61e92f6fbdc6f44
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2025-11-03 15:41:42 +0100

    T/6468 Update __manifest__.py

--- edi_peppol/__manifest__.py
+++ edi_peppol/__manifest__.py
@@ -22,7 +22,7 @@
 # https://www.odoo.com/documentation/14.0/reference/module.html
 #
 {
-    'name': 'EDI Peppol',
+    'name': 'EDI: Peppol',
     'version': '1.0',
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
