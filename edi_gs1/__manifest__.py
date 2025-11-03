47e9a5399f670816ae1ae761e2a14d8353f129de
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2025-11-03 15:41:14 +0100

    T/6468 Update __manifest__.py

--- edi_gs1/__manifest__.py
+++ edi_gs1/__manifest__.py
@@ -22,7 +22,7 @@
 # https://www.odoo.com/documentation/14.0/reference/module.html
 #
 {
-    'name': 'EDI GS1',
+    'name': 'EDI: GS1',
     'version': '1.0',
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
